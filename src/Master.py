66d93edc7c8124999364ebc593cf9840ac6e9211
Rahul Salvi <rsalvi006@me.com>
2016-07-17 19:14:31 -0700

    moved some items around

--- src/Master.py
+++ src/Master.py
@@ -1,5 +1,18 @@
 from ctypes import *
 
+class AxisControl(Union):
+    _fields_ = [
+               ("vel", c_double * 1),
+               ("pos", c_float  * 2)
+               ]
+
+class ControlInput(Structure):
+    _fields_ = [
+               ("angular", AxisControl * 3),
+               ("linear",  AxisControl * 3),
+               ("mode",    c_uint8     * 1)
+               ]
+
 class Goals(Structure):
     _fields_ = [
                ("forwardVision", c_uint8 * 1),
--- src/Navigation.py
+++ src/Navigation.py
@@ -1,18 +1,5 @@
 from ctypes import *
 
-class AxisControl(Union):
-    _fields_ = [
-               ("vel", c_double * 1),
-               ("pos", c_float  * 2)
-               ]
-
-class ControlInput(Structure):
-    _fields_ = [
-               ("angular", AxisControl * 3),
-               ("linear",  AxisControl * 3),
-               ("mode",    c_uint8     * 1)
-               ]
-
 class Kill(Structure):
     _fields_ = [
                ("isKilled", c_bool * 1)
