30081c7fd7bcb56e7f9f682b490bd306ca200cdf
Rahul Salvi <rsalvi006@me.com>
2016-07-17 22:35:12 -0700

    this doesnt work

--- src/Serialization.py
+++ src/Serialization.py
@@ -1,10 +1,14 @@
 from ctypes import *
 
 def Pack(ctype_instance):
-    return string_at(addressof(ctype_instance), sizeof(ctype_instance))
+    buf = string_at(addressof(ctype_instance), sizeof(ctype_instance))
+    string = ''.join([chr(i) for i in buf])
+    return string
 
 def Unpack(ctype, string):
-    bytes = str.encode(string)
-    cstring = create_string_buffer(bytes)
+    # buf = str.encode(string)
+    buf = bytearray()
+    buf.extend(map(ord, string))
+    cstring = create_string_buffer(buf)
     ctype_instance = cast(pointer(cstring), POINTER(ctype)).contents
     return ctype_instance
