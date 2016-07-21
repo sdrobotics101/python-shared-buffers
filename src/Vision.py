739a9b75822d7b626872ab6783e57eca19cfe7dd
Donovan Drews <donovancarldrews@gmail.com>
2016-07-21 04:08:16 +0000

    Added orientation buffer.

--- src/Vision.py
+++ src/Vision.py
@@ -7,3 +7,14 @@ class Location(Structure):
                ("z", c_double),
                ("confidence", c_char),
                ]
+
+class LocationAndRotation(Structure):
+    _fields_ = [
+               ("x", c_double),
+               ("y", c_double),
+               ("z", c_double),
+               ("xrot", c_double),
+               ("yrot", c_double),
+               ("zrot", c_double),
+               ("confidence", c_char),
+               ]
