a7a95cc99a9e2fc4260ce1d92cacf1e469d94f5a
Rahul Salvi <rsalvi006@me.com>
2016-07-23 13:32:29 -0700

    fixed serialization

--- src/Serialization.py
+++ src/Serialization.py
@@ -2,11 +2,13 @@ from ctypes import *
 
 # converts a ctype into a string
 def Pack(ctype_instance):
-    return string_at(addressof(ctype_instance), sizeof(ctype_instance)).decode('latin1')
+    return string_at(addressof(ctype_instance), sizeof(ctype_instance))
 
 # convert from string to ctype
 def Unpack(ctype, string):
-    buf = string.encode('latin1')
+    buf = b''
+    for i in string:
+        buf += i.to_bytes(1, 'little')
     cstring = create_string_buffer(buf)
     ctype_instance = cast(pointer(cstring), POINTER(ctype)).contents
     return ctype_instance
