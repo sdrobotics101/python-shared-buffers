cf9403b4a12603905ffbc0445fcc9277d103dca9
jmagine <jasonma5501@gmail.com>
2019-07-13 19:07:05 -0700

    updated vision buffers

--- shared_buffers/vision.py
+++ shared_buffers/vision.py
@@ -1,27 +1,13 @@
 from ctypes import Structure, c_double, c_uint8, c_char
 
 
-class Location(Structure):
+class Detection(Structure):
     _fields_ = [
         ("x", c_double),
         ("y", c_double),
-        ("z", c_double),
-        ("confidence", c_uint8),
-        ("loctype", c_uint8),
+        ("size", c_uint8),
+        ("class", c_uint8),
     ]
 
-
-class LocationArray(Structure):
-    _fields_ = [("locations", Location * 3)]
-
-
-class LocationAndRotation(Structure):
-    _fields_ = [
-        ("x", c_double),
-        ("y", c_double),
-        ("z", c_double),
-        ("xrot", c_double),
-        ("yrot", c_double),
-        ("zrot", c_double),
-        ("confidence", c_char),
-    ]
+class DetectionArray(Structure):
+    _fields_ = [("detections", Detection * 8)]
