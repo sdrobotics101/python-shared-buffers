0b101fe476f78af3b31a6b2fae66ed56c5bf7d3a
Rahul Salvi <rsalvi006@me.com>
2019-07-25 11:14:19 -0700

    add raw outputs structure to nav

--- shared_buffers/navigation.py
+++ shared_buffers/navigation.py
@@ -1,17 +1,16 @@
-from ctypes import c_bool, c_double, c_uint8, Structure
-
+from ctypes import c_bool, c_double, c_uint8, c_uint16, Structure
 
 class Kill(Structure):
     _fields_ = [("isKilled", c_bool)]
 
-
 class Health(Structure):
     _fields_ = [("saturated", c_uint8), ("direction", c_uint8)]
-
 
 class Outputs(Structure):
     _fields_ = [("motors", c_double * 8)]
 
+class RawOutputs(Structure):
+    _fields_ = [("motors", c_uint16 * 8)]
 
 class PhysicalOutput(Structure):
     _fields_ = [("force", c_double * 3), ("torque", c_double * 3)]
